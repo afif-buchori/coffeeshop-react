bda9315b5d379aa9782518990ce0510b37b9d74f
afif buchori <4fifbuchori@gmail.com>
2023-03-29 15:28:52 +0700

    implementasi state context

--- src/components/Header/account.js
+++ src/components/Header/account.js
@@ -1,44 +1,73 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from "react";
 import iconSearch from "../../assets/icon/search.svg";
 import iconChat from "../../assets/icon/chat.svg";
 import profilePict from "../../assets/user.webp";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+
+import { searchContexts } from "../../utils/contexts";
 
 export class Account extends Component {
   constructor() {
     super();
     this.state = {
       toggleSearch: false,
-    }
+    };
     this.isInput = React.createRef();
   }
   handleToggleSearch(info) {
-    if(this.isInput.current.value) return this.setState({ toggleSearch: true });
+    if (this.isInput.current.value)
+      return this.setState({ toggleSearch: true });
     this.setState({
       toggleSearch: info,
-    })
+    });
   }
   handleSearching = () => {
     console.log(this.isInput.current.value);
     this.props.searchValue(this.isInput.current.value);
-  }
+    this.context.updateSearch(this.isInput.current.value);
+  };
   render() {
+    console.log(this.context.searchContexts);
     return (
       <Fragment>
-        <div onMouseOver={() => this.handleToggleSearch(true)} onMouseOut={() => this.handleToggleSearch(false)} className={this.state.toggleSearch ? "nav-search nav-search-active" : "nav-search"}>
-          <img src={iconSearch} alt="" className={this.state.toggleSearch ? "w-6" : "w-7"} onClick={this.handleSearching} />
-          <input ref={this.isInput} placeholder="Search" className={this.state.toggleSearch ? "block" : "hidden"} />
+        <div
+          onMouseOver={() => this.handleToggleSearch(true)}
+          onMouseOut={() => this.handleToggleSearch(false)}
+          className={
+            this.state.toggleSearch
+              ? "nav-search nav-search-active"
+              : "nav-search"
+          }
+        >
+          <img
+            src={iconSearch}
+            alt=""
+            className={this.state.toggleSearch ? "w-6" : "w-7"}
+            onClick={this.handleSearching}
+          />
+          <input
+            ref={this.isInput}
+            placeholder="Search"
+            className={this.state.toggleSearch ? "block" : "hidden"}
+          />
         </div>
         <div className="relative cursor-pointer">
-          <span className="flex w-4 h-4 rounded-full justify-center items-center text-[8px] text-white bg-secondary absolute -top-2 -left-2">1</span>
+          <span className="flex w-4 h-4 rounded-full justify-center items-center text-[8px] text-white bg-secondary absolute -top-2 -left-2">
+            1
+          </span>
           <img src={iconChat} alt="" className="w-7" />
         </div>
-        <Link to="/profile" className="w-8 h-8 rounded-full border overflow-hidden cursor-pointer">
+        <Link
+          to="/profile"
+          className="w-8 h-8 rounded-full border overflow-hidden cursor-pointer"
+        >
           <img src={profilePict} alt="" />
         </Link>
       </Fragment>
-    )
+    );
   }
 }
 
-export default Account
+Account.contextType = searchContexts;
+
+export default Account;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
-import './styles/index.css';
-import router from './router';
+import "./styles/index.css";
+import router from "./router";
 // import App from './pages/Home';
 // import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    {/* <App name="Afif" age={20} href={"https://reactjs.org"} /> */}
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+import { searchContexts as SearchContexts } from "./utils/contexts";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+function Run({ isStrict, children }) {
+  if (isStrict) return <React.StrictMode>{children}</React.StrictMode>;
+  return children;
+}
+
+function App() {
+  const [searchContexts, setSearchContx] = useState("");
+  const updateSearch = (value) => {
+    setSearchContx(value);
+  };
+  return (
+    <Run isStrict={false}>
+      {/* <App name="Afif" age={20} href={"https://reactjs.org"} /> */}
+      <SearchContexts.Provider value={{ searchContexts, updateSearch }}>
+        <RouterProvider router={router} />
+      </SearchContexts.Provider>
+    </Run>
+  );
+}
+
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
